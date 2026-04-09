e840c2424aebcd4ef31d0fc070a01e54fc214661
puja-03 <pujakumari11th2003@gmail.com>
2026-04-09 22:46:03 +0530

    feat: initialize database schema, models, factories, and seeders for courses, batches, enrollments, and batch teachers

---
+++ database/factories/BatchFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Batch;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Batch>
+ */
+class BatchFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
