445d2949f7f1240318f0fbda28e805e6d874a3c3
puja-03 <pujakumari11th2003@gmail.com>
2026-05-04 15:55:26 +0530

    fix: update UserFactory email generation to use slugified names and random suffixes

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,7 +26,7 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
-            'email' => fake()->unique()->email(),
+            'email' => Str::slug(fake()->name()).'-'.Str::random(8).'@'.fake()->safeEmailDomain(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
